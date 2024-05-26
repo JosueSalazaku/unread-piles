8f1b7c2267cfa7528494963cff9db61792a7f33e
Josué Salazaku <josuesalazaku@gmail.com>
2024-05-26 15:06:39 +0200

    setup users route

--- routes/users.ts
+++ routes/users.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 export const usersRoute = express.Router()
-    .get('/users', (req, res) => {
+    .get('/', (req, res) => {
 
-        // Your code here
-        // This is where you handle the logic for the '/user' endpoint
-        // You can access request parameters using req.params
-        // You can send a response using res.send() or res.json()
-    });
-//is this correct?
+    })
+    .get('/:id', (req, res) => {
+
+    })
+    .post('/', (req, res) => {
+
+    })
+    .put('/:id', (req, res) => {
+
+    })
+    .delete('/', (req, res) => { });
+
