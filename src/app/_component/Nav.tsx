b6e904bd0dc9b45a260d2328fe89ecb9749a108a
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-15 15:27:02 +0200

    update nav ui

--- src/app/_component/Nav.tsx
+++ src/app/_component/Nav.tsx
@@ -1,28 +1,30 @@
 "use client"
 import { useState } from 'react';
-import { SignedIn, SignedOut, UserButton, useAuth } from '@clerk/nextjs';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
+import { Input } from '@/Components/ui/input';
 
 export function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-  const { signOut } = useAuth();
+  // const { signOut } = useAuth();
 
   return (
-    <nav className="flex h-20 w-full items-center justify-between bg-primary px-14">
+    <nav className="flex h-20 w-full items-center justify-between  px-14">
       <Link href="/" className="font-didot text-2xl font-bold text-main">
-        Keanino
+        Unread Piles
       </Link>
       <button onClick={toggle} className="md:hidden">
         {isOpen ? 'Close' : 'Menu'}
       </button>
-      <div className="hidden md:flex flex-row items-center text-white space-x-6">
+      <div className="hidden md:flex flex-row items-center text-black space-x-6">
         <SignedIn>
-          <Link href="/people">People</Link>
-          <Link href="/places">Places</Link>
+          <Link href="/people">Explore</Link>
+          <Link href="/places">Library</Link>
           <Link href="/pages">Pages</Link>
+          <Input className='w-[225px]'/>
           <UserButton />
-          <button onClick={() => signOut()}>Sign Out</button>
+          {/* <button onClick={() => signOut()}>Sign Out</button> */}
         </SignedIn>
         <SignedOut>
           <Link href="/sign-in"><button>Sign In</button></Link>
@@ -34,11 +36,12 @@ export function Nav() {
       {isOpen && (
         <div className="absolute top-20 left-0 right-0 z-50 bg-orange-400 p-5 flex flex-col text-6xl space-y-10 text-main md:hidden">
           <SignedIn>
-            <Link href="/people">People</Link>
-            <Link href="/places">Places</Link>
+            <Link href="/people">Explore</Link>
+            <Link href="/places">Library</Link>
             <Link href="/pages">Pages</Link>
+            <Input/>
             <UserButton />
-            <button onClick={() => signOut()}>Sign Out</button>
+            {/* <button onClick={() => signOut()}>Sign Out</button> */}
           </SignedIn>
           <SignedOut>
             <Link href="/sign-in"><button>Sign In</button></Link>
