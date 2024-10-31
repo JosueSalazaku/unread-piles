f82525aa10e6c6aea966f0fcb483d5c6de92981c
Josué Salazaku <josuesalazaku@gmail.com>
2024-10-31 01:36:24 +0100

    Refactor: Update Tailwind CSS configuration to remove unused plugins and adjust content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-import type { Config } from "tailwindcss";
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
-export default config;
