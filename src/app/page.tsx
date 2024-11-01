d22f49089beb6197e4b2f654e14478e3578e3cf4
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-01 16:29:18 +0100

    Refactor PostCSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import GoogleBooks from "@/components/GoogleBooks";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
+      <SignedOut>
+        <h1>HEYY</h1>
+      </SignedOut>
       <SignedIn>
         <GoogleBooks />
       </SignedIn>
