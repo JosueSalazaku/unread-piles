9e0bb4b3c9ab154a75ce340ff23ac40e2d508389
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-18 21:47:35 +0100

    Refactor: Simplify user sign-in logic in Nav component for improved readability

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,41 +13,41 @@ export function Nav() {
 
   const { name, email, image } = session.data?.user ?? {};
 
-  function checkUserSignIn() {
-    if (session.data?.user) {
-      return (
-        <div className="flex items-center space-x-4">
-          {image && <Image src={image} alt={name ?? "User"} width={40} height={40} className="rounded-full" />}
-        </div>
-      );
-    } else {
-      return (
-        <Link href="/api/auth/sign-in">
-          <button className="font-bold">Login / Sign Up</button>
-        </Link>
-      );
-    }
-  }
-
   return (
     <nav className="flex h-20 w-full items-center justify-between px-6">
-      <Link href="/" className="font-didot text-2xl font-bold text-main">
+      <Link href="/" className="font-didot text-main text-2xl font-bold">
         Unread Piles
       </Link>
       <button onClick={toggle} className="md:hidden">
         {isOpen ? "Close" : "Menu"}
       </button>
-      <div className="hidden md:flex flex-row justify-between items-center font-semibold space-x-6">
+      <div className="hidden flex-row items-center justify-between space-x-6 font-semibold md:flex">
         <SearchBooks />
         <Link href="/explore">Explore</Link>
         <Link href="/library">Library</Link>
         <Link href="/pages">Pages</Link>
-        {checkUserSignIn()}
+        {session.data?.user ? (
+          <div className="flex items-center space-x-4">
+            {image && (
+              <Image
+                src={image}
+                alt={name ?? "User"}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            )}
+          </div>
+        ) : (
+          <Link href="/api/auth/sign-in">
+            <button className="font-bold">Login / Sign Up</button>
+          </Link>
+        )}
       </div>
 
       {/* Small screen */}
       {isOpen && (
-        <div className="absolute top-20 left-0 right-0 z-50 bg-zinc-400 p-5 flex flex-col text-6xl space-y-10 text-main md:hidden">
+        <div className="text-main absolute left-0 right-0 top-20 z-50 flex flex-col space-y-10 bg-zinc-400 p-5 text-6xl md:hidden">
           <SearchBooks />
           <Link href="/people">Explore</Link>
           <Link href="/places">Library</Link>
