a8844f113e81482de8813a17f87633195e02671c
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-18 21:57:48 +0100

    Refactor: Add dropdown menu and signout functionality

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import SearchBooks from "@/components/SearchBooks";
 import { useCustomSession } from "./SessionProvider";
 import Image from "next/image";
+import { signOut } from "@/app/lib/auth-client";
 
 export function Nav() {
   const session = useCustomSession();
-
+  const handleSignOut = () => signOut();
+  
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
@@ -29,19 +31,29 @@ export function Nav() {
         {session.data?.user ? (
           <div className="flex items-center space-x-4">
             {image && (
-              <Image
-                src={image}
-                alt={name ?? "User"}
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
+              <button onClick={toggle}>
+                <Image
+                  src={image}
+                  alt={name ?? "User"}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+              </button>
             )}
           </div>
         ) : (
           <Link href="/api/auth/sign-in">
             <button className="font-bold">Login / Sign Up</button>
           </Link>
+        )}
+        {isOpen && (
+          <div className="absolute right-5 top-16 z-50 flex w-52 flex-col space-y-10 rounded-lg bg-zinc-400">
+            <Link href="/profile">Profile</Link>
+            <Link href="/settings">Settings</Link>
+            <button onClick={handleSignOut}>Sign Out</button>
+
+          </div>
         )}
       </div>
 
