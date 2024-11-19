641a01344a7a0a860116ad40150c9f75eac0a77f
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-19 02:01:30 +0100

    Refactor: Add utility functions for class management; update Tailwind configuration and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,33 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "background-dark": "#110503",
-      "background-light": "#fcf0ee",
-      "dark-brown": "#19100D",
-      "main-orange": "#912b12",
-      "light-orange": "#ed876e",
-      "dark-blue": "#0A054B",
-      "main-blue": "#0008ff",
-      "accent-blue": "#4096d4",
-      
-    }
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	},
+  	colors: {
+  		'background-dark': '#110503',
+  		'background-light': '#fcf0ee',
+  		'dark-brown': '#19100D',
+  		'main-orange': '#912b12',
+  		'light-orange': '#ed876e',
+  		'dark-blue': '#0A054B',
+  		'main-blue': '#0008ff',
+		'accent-blue': '#4096d4'
+	},
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
 export default config;
