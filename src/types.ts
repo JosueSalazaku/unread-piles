c25086b257e51ef2fe7b24339316138b4f1b771d
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-24 21:03:06 +0100

    Refactor: Add books table schema and GoogleBook interface; update server log time and clean up GoogleBooks component

--- src/types.ts
+++ src/types.ts
@@ -55,6 +55,20 @@ export interface Book {
     updatedAt: Date;
 }
 
+export interface GoogleBook {
+    id: string; 
+    volumeInfo: {
+      title: string;
+      authors?: string[]; 
+      publishedDate?: string; 
+      description?: string;
+      imageLinks?: {
+        thumbnail?: string;
+      };
+    };
+  }
+  
+
 export interface VolumeInfo {
     title: string;
     authors?: string[];
