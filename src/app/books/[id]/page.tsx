39906537b50e8f56a29a4dc6998079f6b292c5e2
Josué Salazaku <josuesalazaku@gmail.com>
2024-12-11 22:07:22 +0100

    Refactor: Improve parameter handling and formatting in book detail page

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import type { GoogleBook } from "@/types";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const [book, setBook] = useState<GoogleBook | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const router = useRouter();
-  
+  const { id } = React.use(params);
 
   useEffect(() => {
-    const { id } = params;
+
     if (!id) {
       setError("No books found");
       router.push("/");
@@ -22,13 +22,17 @@ export default function Page({ params }: { params: { id: string } }) {
 
     async function ShowBook() {
       try {
-        const response = await axios.get<GoogleBook>(`https://www.googleapis.com/books/v1/volumes/${params.id}`);
+        const response = await axios.get<GoogleBook>(
+          `https://www.googleapis.com/books/v1/volumes/${id}`,
+        );
         console.log(response.status);
         console.log(response.data);
         setBook(response.data);
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          setError(`Failed to fetch post: ${error.response?.status} - ${error.message}`);
+          setError(
+            `Failed to fetch post: ${error.response?.status} - ${error.message}`,
+          );
         } else {
           setError("Failed to fetch post: An unexpected error occurred");
         }
@@ -58,17 +62,27 @@ export default function Page({ params }: { params: { id: string } }) {
           <div>
             <div>
               <Image
-                src={book?.volumeInfo?.imageLinks?.thumbnail ?? "/default-thumbnail.jpg"}
+                src={
+                  book?.volumeInfo?.imageLinks?.thumbnail ??
+                  "/default-thumbnail.jpg"
+                }
                 alt={book?.volumeInfo?.title || "Book thumbnail"}
                 width={128}
                 height={192}
               />
 
               <h1>{book.volumeInfo.title}</h1>
-              <p>{book.volumeInfo.authors?.join(", ") ?? "Author information not available."}</p>
-              <p>{book.volumeInfo.publishedDate ?? "Publication date not available"}</p>
-              <p>{book.volumeInfo.description ?? "Description not available"}</p>
-
+              <p>
+                {book.volumeInfo.authors?.join(", ") ??
+                  "Author information not available."}
+              </p>
+              <p>
+                {book.volumeInfo.publishedDate ??
+                  "Publication date not available"}
+              </p>
+              <p>
+                {book.volumeInfo.description ?? "Description not available"}
+              </p>
             </div>
           </div>
         )}
