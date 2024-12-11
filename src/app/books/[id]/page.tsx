5588c531f6b9476672afc444498eee9535b798fb
Josué Salazaku <josuesalazaku@gmail.com>
2024-12-11 22:07:58 +0100

    ref: Update dependency in useEffect to include 'id' for correct book loading

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -41,7 +41,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
       }
     }
     void ShowBook();
-  }, [params, router]);
+  }, [id, params, router]);
 
   if (loading) {
     return <div>Loading...</div>;
