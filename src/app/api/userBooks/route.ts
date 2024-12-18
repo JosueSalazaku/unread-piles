655fb8a8fbacf8bceaa0f6c9daa23033e1a1bb8b
Josué Salazaku <josuesalazaku@gmail.com>
2024-12-18 13:16:44 +0100

    feat: Add PATCH endpoint to update user book status #13

--- src/app/api/userBooks/route.ts
+++ src/app/api/userBooks/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/server/db";
 import { userBooks } from "@/server/db/auth-schema";
-import { NextResponse } from "next/server";
+import type { UserBooks } from "@/types";
+import { and, eq } from "drizzle-orm";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
@@ -9,5 +11,18 @@ export async function GET() {
     } catch (error) {
         console.error("Error fetching books:", error);
         return NextResponse.json({ error: "Error fetching books" }, { status: 500 });
+    }
+}
+
+export async function PATCH(req: NextRequest) {
+    const { userId, bookId, status } = await req.json() as UserBooks;
+    try {
+        await db.update(userBooks)
+            .set({ status })
+            .where(and(eq(userBooks.userId, userId), eq(userBooks.bookId, bookId)));
+        return NextResponse.json({ message: "Status updated successfully" });
+    } catch (error) {
+        console.error("Error updating status:", error);
+        return NextResponse.json({ error: "Error updating status" }, { status: 500 });
     }
 }
