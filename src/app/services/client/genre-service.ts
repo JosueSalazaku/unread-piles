4f34af09fa4c6b4547f3c376ef85cade8593f781
Josué Salazaku <josuesalazaku@gmail.com>
2024-12-18 22:52:55 +0100

    feat: Add startIndex parameter to fetchBooksByGenre for pagination support #14

--- src/app/services/client/genre-service.ts
+++ src/app/services/client/genre-service.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 
 const APIKey = process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY;
 
-export const fetchBooksByGenre = async (genre: string): Promise<GoogleBook[]> => {
+export const fetchBooksByGenre = async (genre: string, startIndex = 0): Promise<GoogleBook[]> => {
     if (!APIKey) {
         console.error("Google Books API key is missing.");
         return [];
     }
     try {
         const response = await axios.get<{ items: GoogleBook[] }>(
-            `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=20&key=${APIKey}`
+            `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&startIndex=${startIndex}&maxResults=20&key=${APIKey}`
         );
         return response.data.items || [];
     } catch (error) {
@@ -18,16 +18,3 @@ export const fetchBooksByGenre = async (genre: string): Promise<GoogleBook[]> =>
         return [];
     }
 };
-
-export const fetchFictionBooks = () => fetchBooksByGenre("fiction");
-export const fetchRomanceBooks = () => fetchBooksByGenre("romance");
-export const fetchFantasyBooks = () => fetchBooksByGenre("fantasy");
-export const fetchScienceFictionBooks = () => fetchBooksByGenre("science-fiction");
-export const fetchMysteryBooks = () => fetchBooksByGenre("mystery");
-export const fetchThrillerBooks = () => fetchBooksByGenre("thriller");
-export const fetchHistoryBooks = () => fetchBooksByGenre("history");
-export const fetchHorrorBooks = () => fetchBooksByGenre("horror");
-export const fetchPoetryBooks = () => fetchBooksByGenre("poetry");
-export const fetchBiographyBooks = () => fetchBooksByGenre("biography");
-export const fetchYoungAdultBooks = () => fetchBooksByGenre("young+adults");
-export const fetchEducationBooks = () => fetchBooksByGenre("education");
