c5af83a02a8fd35c551c41b2b47608ab1a9294fd
Josué Salazaku <josuesalazaku@gmail.com>
2024-12-30 14:26:30 +0100

    refactor: Adjust padding in navigation for improved layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -22,7 +22,7 @@ export function Nav() {
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="flex h-16 w-full items-center justify-between border-b-2 border-dark-brown px-4 sm:px-6">
+    <nav className="flex h-16 w-full items-center justify-between border-b-2 border-dark-brown px-3 sm:px-6">
       <Link href="/" className="text-main text-main text-xl font-bold sm:text-2xl">
         <Image
           src="/piles.svg"
