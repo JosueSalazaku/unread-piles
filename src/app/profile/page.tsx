f59b3a1235607d7c0f46986965141846b7d92af1
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-22 23:54:25 +0100

    implement userBooks API route for status

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,18 +10,19 @@ export default function Profile() {
     return <div>You are not logged in. Please sign in to view your information.</div>;
   }
 
-  const {name, email, image} = session.data.user
+  const {name, image} = session.data.user
 
   return (
     <div className="flex flex-col mx-auto max-w-4xl p-6">
-      <div>
+      <div className="flex gap-3 items-center">
         <Image
         src={image ?? "No image"}
         alt={name ?? "No name"}  
-        width={150}
-        height={150}
+        width={100}
+        height={100}
         className="rounded-full"
         />
+        <h1 className="text-xl">{name}</h1>
       </div>
     <UserBooks />
     </div>
