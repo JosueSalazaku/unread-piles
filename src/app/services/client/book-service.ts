098fc3ed35b31722319700ac016da8c41a0c157e
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-22 13:28:18 +0100

    refactor: Simplify fetchAllUserBooks function by removing retry logic and improving error handling

--- src/app/services/client/book-service.ts
+++ src/app/services/client/book-service.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import type { AxiosError } from "axios";
 import type { GoogleBook } from "@/types";
 
 const APIKey = process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY;
@@ -24,7 +23,6 @@ export const fetchBookByInput = async (query: string, startIndex: number, maxRes
         console.error("Google Books API key is missing.");
         return [];
     }
-
     if (!query) {
         console.error("Search Querry is required")
     }
@@ -39,41 +37,17 @@ export const fetchBookByInput = async (query: string, startIndex: number, maxRes
     
 }
 
-const MAX_RETRIES = 5;
-const RETRY_DELAY = 1000; // Initial delay in milliseconds
-
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
 export const fetchAllUserBooks = async (bookId: string) => {
   if (!bookId) {
+    console.error("No BookId found", bookId);
     return null;
   }
-    
-  let attempts = 0;
 
-  while (attempts < MAX_RETRIES) {
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes/${bookId}`
-      );
-      console.log(response.data);
-        return response.data as string;
-   
-        
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      if (axiosError.response && axiosError.response.status === 429) {
-        attempts++;
-        const delay = RETRY_DELAY * Math.pow(2, attempts);
-        console.warn(`Rate limit exceeded. Retrying in ${delay} ms...`);
-        await sleep(delay);
-      } else {
-        console.error(`Error fetching book details for bookId ${bookId}:`, axiosError);
-        return null;
-      }
-    }
+  try {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
+    return response.data as string;
+  } catch (error) {
+      console.error("Error fetching UserBooks by bookId", error)
+    return null;
   }
-
-  console.error(`Failed to fetch book details for bookId ${bookId} after ${MAX_RETRIES} attempts.`);
-  return null;
-};
+}
