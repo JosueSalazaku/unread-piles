22af9a9d1b165eee55f6849fdb8f188803b013bd
Josué Salazaku <josuesalazaku@gmail.com>
2025-03-17 21:39:12 +0100

    Update query keys in useFetchUserBooks and useSaveBooks hooks for clarity

--- src/app/util/hooks/useFetchUserBooks.ts
+++ src/app/util/hooks/useFetchUserBooks.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useFetchUserBooks = (id: string) => {
     return useQuery({
-      queryKey: ["todos"],
+      queryKey: ["userBooks"],
       queryFn: () => fetchUserBooks(id),
     });
   };
